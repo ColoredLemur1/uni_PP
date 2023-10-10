078c801d78b67b5b483f239de04805aa1ad42b49
ColoredLemur1 <102429155+ColoredLemur1@users.noreply.github.com>
2023-10-10 16:02:05 +0000

    Week2 lesson1

---
+++ week2_bootcamp/fourth.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main () {
+    char var[15];
+    printf("Your name: ");
+    scanf("%s", var);
+    printf("Your name is %s\n",var);
+    return 0;
+}
---
+++ week2_bootcamp/week_notes.md
@@ -0,0 +1,5 @@
+gcc -c name of file - name of the program
+
+to copy contents of one file into another use cat and > 
+
+core dump error (forget a & or a * etc)
