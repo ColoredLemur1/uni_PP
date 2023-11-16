76186a077beb9af0dcc473987c348a70846d4b20
ColoredLemur1 <102429155+ColoredLemur1@users.noreply.github.com>
2023-11-16 14:13:45 +0000

    week7_lc1

---
+++ week7_bootcamp/work/empty.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef struct reading{
+    char date[20];
+    float bloodIron;
+} reading;
+
+
+int main(){
+    reading one_reading;
+    reading week_readings[7];
+    
+    return 0;
+}
